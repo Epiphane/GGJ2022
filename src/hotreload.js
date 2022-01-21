282f25cb5db440315ee8ad6ca96166b15b514a66
Epiphane <exyphnos@gmail.com>
2022-01-21 04:29:53 +0000

    Deploying to gh-pages from @ Epiphane/GGJ2022@1bb66ac0a00eb1659d019ea9bc51bf8f4c9492bf 🚀

---
+++ src/hotreload.js
@@ -0,0 +1,24 @@
+"use strict";
+if (window.location.host.indexOf('localhost') >= 0) {
+    const socket = new WebSocket('ws://localhost:8081');
+    socket.addEventListener('close', () => {
+        window.location.reload();
+    });
+}
+else if (fetch) {
+    let start;
+    const test = () => fetch(`./version.js?${Date.now()}`)
+        .then(d => d.text())
+        .then(s => {
+        if (!start) {
+            start = s;
+        }
+        else if (start !== s) {
+            clearInterval(interval);
+            document.getElementById('update').style.display = 'block';
+        }
+    });
+    test();
+    const interval = setInterval(test, 5000);
+}
+//# sourceMappingURL=hotreload.js.map
